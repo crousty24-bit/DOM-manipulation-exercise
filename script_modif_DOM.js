4eecc66ebd3274c37b1d144b1a737d91c7525272
crousty24-bit <allen.kch24@gmail.com>
2026-01-21 15:33:22 +0100

    add first func to change text content to 2 different class

---
+++ script_modif_DOM.js
@@ -0,0 +1,12 @@
+// 1. changeTitles
+function changeTitles(){
+  document.querySelector(".jumbotron-heading").textContent = "Ce que j'ai appris à THP"
+  document.getElementsByClassName("lead text-muted")[0].textContent = "THP est une formation qui, en 3 mois, à plein temps, vous apportera des connaissances actionnables pour vous permettre de voir plus loin. Chez nous, pas de professeurs, pas de locaux, mais un groupe de travail en présentiel. Après 11 semaines, les principaux langages et outils du web n'auront plus de secret pour vous !"
+}
+changeTitles();
+
+// 2. changeCallToAction
+function changeCallToAction(){
+  
+}
+changeCallToAction();
